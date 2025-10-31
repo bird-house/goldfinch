eefae1ba1b74e919c894f57fb316e5b97549bb28
David Huard <huard.david@ouranos.ca>
2025-10-31 15:53:35 -0400

    clean-up, add some doc, add tests for individual commands.

--- chain/test.py
+++ chain/test.py
@@ -7,24 +7,27 @@ from xclim.testing.helpers import test_timeseries as tt
 from chain import cli
 
 
-@pytest.fixture
+@pytest.fixture(scope="module")
 def tas_series():
     """Return mean temperature time series."""
     _tas_series = partial(tt, variable="tas")
     return _tas_series
 
 
-def test_chain(tas_series, tmp_path):
+@pytest.fixture(scope="module")
+def input_file(tas_series, tmpdir_factory):
     # Create input file
+    input_file = str(tmpdir_factory.mktemp("input").join("in.nc"))
     tas = tas_series(np.ones(366) + 271.15, start="1/1/2000")
     tas = tas.expand_dims(dim={"lon": np.linspace(-80, -70, 10), "lat": np.linspace(40, 50, 10)},)
     tas.lon.attrs["standard_name"] = "longitude"
     tas.lat.attrs["standard_name"] = "latitude"
     ds = xr.Dataset(data_vars={"tas": tas})
-    
-    input_file = tmp_path / "in.nc"
     ds.to_netcdf(input_file, engine="h5netcdf")
+    return input_file
 
+
+def test_chain(input_file, tmp_path):
     output_file = tmp_path / "out.nc"
     args = [str(input_file), 
             str(output_file),
@@ -38,9 +41,52 @@ def test_chain(tas_series, tmp_path):
     assert results.exit_code == 0
 
     if output_file.exists():
-        print(output_file)
         out = xr.open_dataset(str(output_file), engine="h5netcdf")
         outvar = list(out.data_vars.values())[0]
         np.testing.assert_allclose(outvar[0], 6588.0)
     else:
         raise FileNotFoundError
+    
+
+def test_hdd(input_file, tmp_path):    
+    output_file = tmp_path / "out.nc"
+    args = [str(input_file), 
+            str(output_file),
+            "hdd", 
+            "--thresh", "17 degC"]
+    runner = CliRunner()
+    results = runner.invoke(cli, args)
+    
+    assert results.exit_code == 0
+
+    if output_file.exists():
+        out = xr.open_dataset(str(output_file), engine="h5netcdf")
+        outvar = list(out.data_vars.values())[0]
+        np.testing.assert_allclose(outvar[0], 6588.0)
+    else:
+        raise FileNotFoundError
+    
+
+def test_subset(input_file, tmp_path):
+    output_file = tmp_path / "out.nc"
+    args = [str(input_file), 
+            str(output_file),
+            "subset", 
+            "-p", "small_geojson.json",
+            "-s", "2000-06",
+            "-e", "2000-08",]
+    runner = CliRunner()
+    results = runner.invoke(cli, args)
+    
+    assert results.exit_code == 0
+
+    if output_file.exists():
+        out = xr.open_dataset(str(output_file), engine="h5netcdf")
+        assert "tas" in out.data_vars
+        assert out.time.isel(time=0) == np.datetime64("2000-06-01T00:00:00")
+        assert out.time.isel(time=-1) == np.datetime64("2000-08-31T00:00:00")
+        assert len(out.tas.lon) == 1
+        assert len(out.tas.lat) == 1
+    else:
+        raise FileNotFoundError
+    
